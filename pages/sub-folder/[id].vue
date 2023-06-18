5fb74e6712e885abfb74a6f4863411afe9a65fb9
redbios <as.redbios@gmail.com>
2023-06-19 06:53:35 +0700

    update

---
+++ pages/sub-folder/[id].vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Hello from params id with : {{ route.params.id }}</h1>
+    <h1>Hello from query with : {{ route.query }}</h1>
+  </div>
+</template>
+
+<script setup>
+const route = useRoute()
+</script>
